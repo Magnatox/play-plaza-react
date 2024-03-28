61b44516368008fb50cfd30baa9ebf833b8bd8ef
Magnatox <19032002i@gmail.com>
2024-03-28 03:29:14 +0300

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -4,9 +4,10 @@ import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
   const {data, error, isLoading} = useGenres();
   if (error) return null;
   if (isLoading) return <Spinner />;
@@ -20,7 +21,12 @@ const GenreList = ({onSelectGenre}: Props) => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Button onClick={() => onSelectGenre(genre)} fontSize="sm" variant="link">
+            <Button
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+              onClick={() => onSelectGenre(genre)}
+              fontSize="sm"
+              variant="link"
+            >
               {genre.name}
             </Button>
           </HStack>
