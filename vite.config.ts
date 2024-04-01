8557e65c687228ad22bd90e9441388538454b5c5
Magnatox <19032002i@gmail.com>
2024-04-01 23:31:45 +0300

    Updaing base to full repository adress

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/play-plaza-react/",
   plugins: [react()],
+  base: "https://magnatox.github.io/play-plaza-react/",
 });
