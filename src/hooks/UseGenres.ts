d9360d30712452d28a6bac60034f1153447499b1
Magnatox <19032002i@gmail.com>
2024-04-11 02:08:37 +0300

    Refactor: Fetching genres with react query

--- src/hooks/UseGenres.ts
+++ src/hooks/UseGenres.ts
@@ -1,13 +1,18 @@
+import {useQuery} from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import {FetchRespons} from "./useData";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => ({
-  data: genres,
-  isLoading: false,
-  error: null,
-});
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () => apiClient.get<FetchRespons<Genre>>("/genres").then((res) => res.data),
+    staleTime: 1 * 1000,
+    initialData: {count: genres.length, results: genres},
+  });
 export default useGenres;
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import apiClient from "../services/api-client";
 import {AxiosRequestConfig, CanceledError} from "axios";
 
-interface FetchRespons<T> {
+export interface FetchRespons<T> {
   count: number;
   results: T[];
 }
