92a5dbba76a99717ff06600d5a5b7599d7b7e03f
Magnatox <19032002i@gmail.com>
2024-04-14 23:58:55 +0300

    Change cache time

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -14,7 +14,7 @@ const usePlatforms = () =>
   useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 1 * 1000,
+    staleTime: 24 * 60 * 60 * 1000,
     initialData: {count: platforms.length, results: platforms},
   });
 
